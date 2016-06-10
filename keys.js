54be3bf07d148cf52e670a4dc8f6288668ecb4ff
Octavio Mata <o.mata07@me.com>
2016-06-10 18:20:15 -0400

    added twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'yzjAz12LxbNafzxGMohiGhZmf',
+  consumer_secret: 'PgBBUjReA1S6gQE5DfaOZAe7OZadetv5RHPp6ER6tjAC6Ws4Bb',
+  access_token_key: '<854845855-h3TuV5ochhF8nBdflAlsAk1afO8VbpgXD6K5337j>',
+  access_token_secret: '<0lbKmv4tbrBJFdesaDxzUlYiJLY4wkPPV1nrjvbVejkRy>',
 }
